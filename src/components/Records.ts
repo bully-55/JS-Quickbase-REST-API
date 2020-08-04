a7bb8201966b03823f0b1a4d5d3e4ae946e3d3b4
Oleg <oleg@qubitronic.com>
2020-08-04 15:34:59 +0300

    add update and get for records

--- src/components/Records.ts
+++ src/components/Records.ts
@@ -1,38 +1,76 @@
-import { RecordData, DeleteRecordResponse } from "../typings"
+import { 
+    DeleteRecordResponse, 
+    QueryRecordData, 
+    QueryRecordDataResponse,
+    UpdateCreateRecordResponse,
+} from "../typings"
 
 class Records {
 
     private url: string
+    private queryUrl: string
     // private headers: object
     
     constructor(headers) {
         // this.headers = headers
         this.url = "https://api.quickbase.com/v1/records"
+        this.queryUrl = "https://api.quickbase.com/v1/records/query"
     }
 
-    create = async (data: RecordData, headers: object): Promise<object> => {
+    create = async (to: string, data: object[], headers: object): Promise<UpdateCreateRecordResponse> => {
         return new Promise((resolve, reject) => {
             fetch(this.url, {
                 method: "POST",
                 mode: "cors",
                 headers: headers as HeadersInit,
-                body: JSON.stringify(data),
-            }).then(res => {
-                return res.json
-            }).then(res => {
+                body: JSON.stringify({ to, data }),
+            }).then((res: Response) => {
+                return res.json()
+            }).then((res: UpdateCreateRecordResponse) => {
                 resolve(res)
-            }).catch(err => {
+            }).catch((err: Error) => {
                 reject(err)
             })
         })
     }
 
-    delete = (from: string, where: string, headers: object): Promise<Response> => {
-        return fetch(this.url, {
-            method: "POST",
-            mode: "cors",
-            headers: headers as HeadersInit,
-            body: JSON.stringify({ from, where }),
+    update = async (rids: number[], data: RecordData): Promise<UpdateCreateRecordResponse> => {
+        return new Promise((resolve, reject) => {
+            fetch(this.url)
+        })
+    }
+
+    delete = async (from: string, where: string, headers: object): Promise<DeleteRecordResponse> => {
+        return new Promise((resolve, reject) => {
+            fetch(this.url, {
+                method: "POST",
+                mode: "cors",
+                headers: headers as HeadersInit,
+                body: JSON.stringify({ from, where }),
+            }).then((res: Response) => {
+                return res.json()
+            }).then((res: DeleteRecordResponse) => {
+                resolve(res)
+            }).catch((err: Error) => {
+                reject(err)
+            })
+        })
+    }
+
+    get = async (opt: QueryRecordData, headers): Promise<QueryRecordDataResponse> => {
+        return new Promise((resolve, reject) => {
+            fetch(this.queryUrl, {
+                method: "POST",
+                mode: "cors",
+                headers: headers as HeadersInit,
+                body: JSON.stringify(opt),
+            }).then((res: Response) => {
+                return res.json()
+            }).then((res: QueryRecordDataResponse) => {
+                resolve(res)
+            }).catch((err: Error) => {
+                reject(err)
+            })
         })
     }
 
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,8 +1,31 @@
-export type RecordData = {
-    to: string,
-    data: object[]
+// export type RecordData = {
+//     to: string,
+//     data: object[]
+// }
+
+export type UpdateCreateRecordResponse = {
+    data: any[],
+    metadata: { createRecordIds: number[] },
+    totalNumberOfRecordsProcessed: number,
+    unchangedRecordIds: any[],
+    updatedRecordIds: number[]
 }
 
 export type DeleteRecordResponse = {
     numberDeleted: number
+}
+
+export type QueryRecordData = {
+    to: string,
+    select?: number[],
+    where?: string,
+    sortBy?: { fieldId: number, order: string }[],
+    groupBy?: { fieldId: number, grouping: string }[],
+    options?: { skip: number, top: number },
+}
+
+export type QueryRecordDataResponse = {
+    data: object[],
+    fields: { id: number, label: string, type: string }[],
+    metadata: { totalRecords: number, numRecords: number, numFields: number, skip: number }
 }
